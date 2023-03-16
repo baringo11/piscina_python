ab7b4bde5ad946ca76b3a0efa766527fb6907aa1
javi <javi.baringo@gmail.com>
2023-03-16 18:29:01 +0100

    almost finished

--- module_01/ex02/test.py
+++ module_01/ex02/test.py
@@ -1,13 +1,61 @@
 from vector import Vector
 
 if __name__ == "__main__" :
-    prueba = Vector([[1., 2., 3.]])
-    prueba1 = Vector([[3., 6., 3.]])
-    prueba2 = Vector([[1.],[2.], [3.]])
+    # CONSTRUCTOR
+    print("-----CONSTRUCTOR-----")
+    filaVec = Vector([[1., 2., 3.]])
+    colVec = Vector([[1.],[2.], [3.]])
+    print(str(filaVec) + "   " + str(filaVec.shape))
+    print(str(colVec) + "   " + str(colVec.shape))
 
-    print(prueba.shape)
-    print(prueba2.shape)
+    rangoVec = Vector(6)
+    rangoVec2 = Vector((5,10))
+    print(str(rangoVec) + "   " + str(rangoVec.shape) )
+    print(str(rangoVec2) + "   " + str(rangoVec2.shape))
 
-    prueba.dot(prueba1)
+    # SUMA
+    print("\n-----SUMA-----")
+    filaVec1 = Vector([[4., 5., 6.]])
+    colVec2 = Vector([[4.],[5.], [6.]])
+    print(filaVec + filaVec1)
+    print(colVec + colVec2)
+
+    print(filaVec + 5)
+    print(colVec + 5)
+
+    # MULTIPLICACION
+    print("\n-----MULTIPLICACION-----")
+
+    print(filaVec * filaVec1)
+    print(colVec * colVec2)
+
+    print(filaVec * 5)
+    print(colVec * 5)
+    #prueba.dot(prueba1)
    # prueba2 = Myvector("fdsdf")
+
+    """
+    # Column vector of shape n * 1
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v2 = v1 * 5
+    print(v2)
+    # Expected output:
+    # Vector([[0.0], [5.0], [10.0], [15.0]])
+    # Row vector of shape 1 * n
+    v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+    v2 = v1 * 5
+    print(v2)
+    # Expected output
+    # Vector([[0.0, 5.0, 10.0, 15.0]])
+    v2 = v1 / 2.0
+    print(v2)
+    # Expected output
+    # Vector([[0.0], [0.5], [1.0], [1.5]])
+    v1 / 0.0
+    # Expected ouput
+    # ZeroDivisionError: division by zero.
+    2.0 / v1
+    # Expected output:
+    # NotImplementedError: Division of a scalar by a Vector is not defined here.
+    """
     
--- module_01/ex02/vector.py
+++ module_01/ex02/vector.py
@@ -1,29 +1,54 @@
 class Vector :
     def __init__(self, values) :
-        elements = 0
-        vector = []
         if isinstance(values, list) :
-            for i in values :
-                if isinstance(i, list) :
-                    if len(i) == 1 :
-                        if not isinstance(i[0], float) :
-                            raise ValueError("should be a float")
-                        vector.append(i[0])  # vector columna [[1.], [2.], [3.]]
-                        self.shape = (len(vector), 1)
-                    else :
-                        for f in i :
-                            if not isinstance(f, float) :
-                                raise ValueError("should be floats")
-                            vector.append(f)  # vector fila [[1., 2., 3.]]
-                            self.shape = (1, len(vector))
-                else :
-                    raise ValueError("should be a nested list")
+            if all(isinstance(i, list) and len(i) == 1 for i in values) :
+                self.values = values # vector columna [[1.], [2.], [3.]]
+                self.shape = (len(values), 1)
+            else :
+                self.values = values # vector fila [[1., 2., 3.]]
+                self.shape = (1, len(values[0]))
+        elif isinstance(values, tuple):
+            if values[0] > values[1]:
+                raise ValueError("Invalid range: the first value must be less than the second value.")
+            self.values = [[float(i)] for i in range(values[0], values[1])]
+            self.shape = (len(self.values), 1)
+        elif isinstance(values, int):
+            self.values = [[float(i)] for i in range(values)]
+            self.shape = (values, 1)
         else :
-            raise ValueError("should be a list")
-        self.values = vector
+            raise ValueError("Invalid arg for constructing Vector")
 
     def dot(self, vector) :
         if self.shape != vector.shape :
-            print("different shape")
-            return 1
-        print (self.shape[0] * vector.shape[0] + self.shape[1] * vector.shape[1])
+            raise ValueError("Shapes aren't equal")
+        return self * vector
+
+    def T(self) :
+        result = []
+        for i in range(self.shape[1]):
+            row = []
+            for j in range(self.shape[0]):
+                row.append(self.values[j][i])
+            result.append(row)
+        return Vector(result)
+
+    def __str__(self):
+        return str(self.values)
+
+    def __add__(self, other):
+        if isinstance(other, (int, float)) :
+            other = Vector([[other] * self.shape[1]] * self.shape[0])
+        if isinstance(other, Vector) :
+            if self.shape != other.shape:
+                raise ValueError("Shapes aren't equal")
+            return Vector([[x + y for x, y in zip(a, b)] for a, b in zip(self.values, other.values)])
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)) :
+            other = Vector([[other] * self.shape[1]] * self.shape[0])
+        if isinstance(other, Vector) :
+            if self.shape != other.shape:
+                raise ValueError("Shapes aren't equal")
+            return Vector([[x * y for x, y in zip(a, b)] for a, b in zip(self.values, other.values)])
+
+    
