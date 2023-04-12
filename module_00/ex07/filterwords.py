2c8c15cfa1dca03d13421fb2bad903397f8ef622
Javier Baringo romero <jbaringo@c1r16s1.42madrid.com>
2023-04-12 15:11:05 +0200

    done

--- module_00/ex07/filterwords.py
+++ module_00/ex07/filterwords.py
@@ -5,19 +5,20 @@ import re
 def filter(text, n) :
     
     words = []
+    punct_chars = f"[{re.escape(string.punctuation)}]"
     for word in text.split() :
         i = 0
         for char in word :
             if char not in string.punctuation :
                 i += 1
-        word = re.sub(f"[{re.escape(string.punctuation)}]", "", word)
+        word = re.sub(punct_chars, "", word)
         if (i > n) :
             words.append(word)
 
     print(words)
 
 if __name__ == "__main__" :
-    if (len(sys.argv) != 3 or not (sys.argv[2].isdigit())) :
+    if (len(sys.argv) != 3 or not(not sys.argv[1].isdigit() and (sys.argv[2].isdigit()))) :
         print("Error, bad arguments")
     else :
         filter(sys.argv[1], int(sys.argv[2]))
