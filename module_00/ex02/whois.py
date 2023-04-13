df73217d7bfa8f0ebe9d245442953632fbeb929f
Javier Baringo romero <jbaringo@c2r2s4.42madrid.com>
2023-04-13 11:21:27 +0200

    works for negative numbers

--- module_00/ex02/whois.py
+++ module_00/ex02/whois.py
@@ -2,14 +2,15 @@ import sys
 
 len = len(sys.argv)
 if (len > 2) :
-    exit(print('AssertionError: more than one argument are provided'))
-if (len == 2 and sys.argv[1].isdigit()) :
-    n = int(sys.argv[1])
-    if (n % 2) :
-        print('I\'m Odd.')
-    elif (n == 0) :
-        print('I\'m Zero.')
-    else :
-        print('I\'m Even.')
-elif (len != 1) :
-    print('AssertionError: argument is not an integer')
+    exit(print('Error: more than one argument are provided'))
+if (len == 2):
+    try:
+        n = int(sys.argv[1])
+        if (n % 2) :
+            print('I\'m Odd.')
+        elif (n == 0) :
+            print('I\'m Zero.')
+        else :
+            print('I\'m Even.')
+    except:
+        print('Error: argument is not an integer')
